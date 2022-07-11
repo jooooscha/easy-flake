0397cc924b2e3f585ac3de93dd3753679595ef27
Joscha Loos <j.loos@posteo.net>
2022-07-11 15:39:48 +0200

    fix: python module

--- flake.nix
+++ flake.nix
@@ -23,6 +23,7 @@
       };
       naersk-lib = naersk.lib.${system};
       rust-module = import ./modules/rust.nix { inherit pkgs flake-utils; lib = pkgs.lib; };
+      python-module = import ./modules/python.nix { inherit pkgs flake-utils; lib = pkgs.lib; };
     in {
       pkgs = pkgs;
 
